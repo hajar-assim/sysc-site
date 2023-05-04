155a0c090ff34098da7c9b868f7b167295fde852
yusufibrahim254 <yusufmostafa0@gmail.com>
2023-05-03 23:48:22 -0400

    new changes to navbar + new references tab for FAQ and Usefull links added.

--- src/pages/home/navBar/NavBar.js
+++ src/pages/home/navBar/NavBar.js
@@ -6,10 +6,11 @@ const NavBar = () => {
     return(
         <div className="navbar">
             <div className="rightside"></div>
-                <Link to="/">home</Link>
-                <Link to="/years">years</Link>
-                <Link to="/programtree">program trees</Link>
-                <Link to="/calculator">calculator</Link>
+                <Link to="/">HOME</Link>
+                <Link to="/years">YEARS</Link>
+                <Link to="/programtree">PROGRAM TREES</Link>
+                <Link to="/calculator">CALCULATOR</Link>
+                <Link to="/references">REFERENCES</Link>
         </div>
     );
 }
